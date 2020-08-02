5f62484ede3cc3360f026270c329697a5bb20e36
aspirine228 <alexn_1999@mail.ru>
2020-08-02 16:01:33 +0300

    x

---
+++ RuinUCHM/RuinUCHM/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+$('#myButton').click(function () {
+    $.scrollTo($('container'), 1000);
+});
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
